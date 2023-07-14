4818f4a7bd1c947bedc56d1d3a43158a0ef6a239
SonaliN84 <sonali.nilekar@gmail.com>
2023-07-14 23:54:42 +0530

    updated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route,Redirect } from "react-router-dom";
 import "./App.css";
 import RootLayout from "./Pages/RootLayout";
 import Signup from "./Pages/Signup";
